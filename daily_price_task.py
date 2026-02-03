d4d5f44128ec2ac6be5b177341879a3e60f2b8d1
Devin <devin.ahmad@appfuxion.id>
2026-02-03 16:06:32 +0700

    add developer and community data as ndjson

--- daily_price_task.py
+++ daily_price_task.py
@@ -11,6 +11,73 @@ import pandas as pd
 load_dotenv()
 
 
+def process_market_data(data, df):
+    df_tmp = pd.DataFrame()
+    df_tmp["current_price"] = [
+        value for value in data["market_data"]["current_price"].values()
+    ]
+    df_tmp["market_cap"] = [
+        value for value in data["market_data"]["market_cap"].values()
+    ]
+    df_tmp["total_volume"] = [
+        value for value in data["market_data"]["total_volume"].values()
+    ]
+    df_tmp["currency"] = [
+        value for value in data["market_data"]["current_price"].keys()
+    ]
+    df_tmp["batch_id"] = batch_id
+    df_tmp["crypto_id"] = data["id"]
+    df_tmp["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    market_data_df = pd.concat([df, df_tmp], ignore_index=True)
+
+    return market_data_df
+
+
+def process_community_data(data, df):
+    columns = [
+        "facebook_likes",
+        "reddit_average_posts_48h",
+        "reddit_average_comments_48h",
+        "reddit_subscribers",
+        "reddit_accounts_active_48h",
+    ]
+    df_tmp = pd.DataFrame()
+    for c in columns:
+        df_tmp.loc[0, c] = data["community_data"][c]
+    df_tmp["batch_id"] = batch_id
+    df_tmp["crypto_id"] = data["id"]
+    df_tmp["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    community_data_df = pd.concat([df, df_tmp], ignore_index=True)
+
+    return community_data_df
+
+
+def process_developer_data(data, df):
+    columns = [
+        "forks",
+        "stars",
+        "subscribers",
+        "total_issues",
+        "closed_issues",
+        "pull_requests_merged",
+        "pull_request_contributors",
+        "code_additions_deletions_4_weeks",
+        "commit_count_4_weeks",
+    ]
+    df_tmp = pd.DataFrame()
+    for c in columns:
+        df_tmp.loc[0, c] = str(data["developer_data"][c])
+    df_tmp["batch_id"] = batch_id
+    df_tmp["crypto_id"] = data["id"]
+    df_tmp["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    developer_data_df = pd.concat([df, df_tmp], ignore_index=True)
+
+    return developer_data_df
+
+
 def daily_api_call(crypto_list: list, batch_id: str = "7AM"):
     # Get current datetime and format it as YYYY-MM-DD
     current_date = datetime.now().strftime("%Y-%m-%d")
@@ -26,6 +93,34 @@ def daily_api_call(crypto_list: list, batch_id: str = "7AM"):
             "total_volume",
         ]
     )
+    community_data_df = pd.DataFrame(
+        columns=[
+            "timestamp",
+            "batch_id",
+            "crypto_id",
+            "facebook_likes",
+            "reddit_average_posts_48h",
+            "reddit_average_comments_48h",
+            "reddit_subscribers",
+            "reddit_accounts_active_48h",
+        ]
+    )
+    developer_data_df = pd.DataFrame(
+        columns=[
+            "timestamp",
+            "batch_id",
+            "crypto_id",
+            "forks",
+            "stars",
+            "subscribers",
+            "total_issues",
+            "closed_issues",
+            "pull_requests_merged",
+            "pull_request_contributors",
+            "code_additions_deletions_4_weeks",
+            "commit_count_4_weeks",
+        ]
+    )
     for crypto in crypto_list:
         url = f"https://api.coingecko.com/api/v3/coins/{crypto}/history?date={current_date}&localization=true"
         api_key = os.getenv("COINGECKO_API_KEY")
@@ -36,32 +131,24 @@ def daily_api_call(crypto_list: list, batch_id: str = "7AM"):
         response = requests.get(url, headers=headers)
 
         data = response.json()
+        market_data_df = process_market_data(data, market_data_df)
+        community_data_df = process_community_data(data, community_data_df)
+        developer_data_df = process_developer_data(data, developer_data_df)
 
-        df_tmp = pd.DataFrame()
-
-        df_tmp["current_price"] = [
-            value for value in data["market_data"]["current_price"].values()
-        ]
-        df_tmp["market_cap"] = [
-            value for value in data["market_data"]["market_cap"].values()
-        ]
-        df_tmp["total_volume"] = [
-            value for value in data["market_data"]["total_volume"].values()
-        ]
-        df_tmp["currency"] = [
-            value for value in data["market_data"]["current_price"].keys()
-        ]
-        df_tmp["batch_id"] = batch_id
-        df_tmp["crypto_id"] = data["id"]
-        df_tmp["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M")
-
-        market_data_df = pd.concat([market_data_df, df_tmp], ignore_index=True)
-
-    market_data_df.to_csv(
-        f"output/crypto_data_{current_date}_BATCH_{batch_id}.csv", index=False
-    )
     market_data_df.to_json(
         f"output/crypto_data_{current_date}_BATCH_{batch_id}.ndjson",
+        orient="records",
+        lines=True,
+    )
+
+    community_data_df.to_json(
+        f"output/community_data_{current_date}_BATCH_{batch_id}.ndjson",
+        orient="records",
+        lines=True,
+    )
+
+    developer_data_df.to_json(
+        f"output/developer_data_{current_date}_BATCH_{batch_id}.ndjson",
         orient="records",
         lines=True,
     )
